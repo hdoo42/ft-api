77b6eb42a0cd0013d100027c48cb2c7e0e6412a2
Hyeokjin <tyeirdoo06@gmail.com>
2024-04-20 00:51:49 +0900

    basic http connector, client, session

--- src/common/client.rs
+++ src/common/client.rs
@@ -1,5 +1,7 @@
 use futures::{future::BoxFuture, FutureExt};
+use lazy_static::lazy_static;
 use rsb_derive::Builder;
+use serde::{Deserialize, Serialize};
 use std::fmt::Formatter;
 use std::{error::Error, time::Duration};
 use std::{fmt::Display, sync::Arc};
@@ -8,7 +10,7 @@ use url::{ParseError, Url};
 use reqwest::StatusCode;
 use util::config_env_var;
 
-use crate::{AccessToken, AuthInfo, FtLocations};
+use crate::{AccessToken, AuthInfo};
 
 #[derive(Clone, Debug)]
 pub struct FtClient<FCHC>
@@ -37,47 +39,6 @@ where
     }
 }
 
-struct FtApiClientHttpSessionApi<FCHC>
-where
-    FCHC: FtClientHttpConnector + Send,
-{
-    token: AccessToken,
-    client: FtClient<FCHC>,
-}
-
-#[derive(Debug)]
-pub enum FtClientError {
-    ApiError(FtClientApiError),
-    HttpError(FtClientHttpError),
-    HttpProtocolError(FtClientHttpProtocolError),
-    EndOfStream(FtClientEndOfStreamError),
-    SystemError(FtClientSystemError),
-    ProtocolError(FtClientProtocolError),
-    RateLimitError(FtRateLimitError),
-}
-
-impl FtClientError {
-    fn option_to_string<T: ToString>(value: &Option<T>) -> String {
-        value
-            .as_ref()
-            .map_or_else(|| "-".to_string(), |v| v.to_string())
-    }
-}
-
-impl Display for FtClientError {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        match *self {
-            FtClientError::ApiError(ref err) => err.fmt(f),
-            FtClientError::HttpError(ref err) => err.fmt(f),
-            FtClientError::HttpProtocolError(ref err) => err.fmt(f),
-            FtClientError::EndOfStream(ref err) => err.fmt(f),
-            FtClientError::SystemError(ref err) => err.fmt(f),
-            FtClientError::ProtocolError(ref err) => err.fmt(f),
-            FtClientError::RateLimitError(ref err) => err.fmt(f),
-        }
-    }
-}
-
 pub type ClientResult<T> = std::result::Result<T, FtClientError>;
 
 impl<FCHC> FtClient<FCHC>
@@ -91,8 +52,19 @@ where
     }
 }
 
-impl FtApiClientHttpSessionApi {
-    async fn new() -> Self {
+pub struct FtApiClientHttpSessionApi<FCHC>
+where
+    FCHC: FtClientHttpConnector + Send,
+{
+    token: AccessToken,
+    pub client: FtClient<FCHC>,
+}
+
+impl<FCHC> FtApiClientHttpSessionApi<FCHC>
+where
+    FCHC: FtClientHttpConnector + Send + Sync,
+{
+    pub async fn new(http_connector: FCHC) -> Self {
         let token = AccessToken::try_get(AuthInfo::from_env(
             config_env_var("FT_API_CLIENT_UID").unwrap(),
             config_env_var("FT_API_CLIENT_SECRET").unwrap(),
@@ -104,20 +76,30 @@ impl FtApiClientHttpSessionApi {
 
         FtApiClientHttpSessionApi { token, client }
     }
+}
 
-    async fn campus_gs_locations(&self) -> Vec<FtLocations> {
-        self.client.get("")
-    }
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct FtEnvelopeMessage {
+    pub ok: bool,
+    pub error: Option<String>,
+    // Slack may return validation errors in `errors` field with `ok: false` for some methods (such as `apps.manifest.validate`.
+    pub errors: Option<Vec<String>>,
+    pub warnings: Option<Vec<String>>,
 }
 
 pub trait FtClientHttpConnector {
-    fn http_get_uri<'a, RS>(&'a self, full_uri: Url) -> BoxFuture<'a, ClientResult<RS>>
+    fn http_get_uri<'a, RS>(
+        &'a self,
+        full_uri: Url,
+        token: &'a AccessToken,
+    ) -> BoxFuture<'a, ClientResult<RS>>
     where
         RS: for<'de> serde::de::Deserialize<'de> + Send + 'a + 'a + Send;
 
     fn http_get<'a, 'p, RS, PT, TS>(
         &'a self,
         method_relative_uri: &str,
+        token: &'a AccessToken,
         params: &'p PT,
     ) -> BoxFuture<'a, ClientResult<RS>>
     where
@@ -130,7 +112,7 @@ pub trait FtClientHttpConnector {
             .and_then(|url| FtClientHttpApiUri::create_url_with_params(url, params));
 
         match full_uri {
-            Ok(full_uri) => self.http_get_uri(full_uri),
+            Ok(full_uri) => self.http_get_uri(full_uri, token),
             Err(err) => std::future::ready(Err(err)).boxed(),
         }
     }
@@ -138,6 +120,7 @@ pub trait FtClientHttpConnector {
     fn http_post_uri<'a, RQ, RS>(
         &'a self,
         full_uri: Url,
+        token: &'a AccessToken,
         request_body: &'a RQ,
     ) -> BoxFuture<'a, ClientResult<RS>>
     where
@@ -147,6 +130,7 @@ pub trait FtClientHttpConnector {
     fn http_post<'a, RQ, RS>(
         &'a self,
         method_relative_uri: &str,
+        token: &'a AccessToken,
         request: &'a RQ,
     ) -> BoxFuture<'a, ClientResult<RS>>
     where
@@ -154,7 +138,7 @@ pub trait FtClientHttpConnector {
         RS: for<'de> serde::de::Deserialize<'de> + Send + 'a,
     {
         match self.create_method_uri_path(method_relative_uri) {
-            Ok(full_uri) => self.http_post_uri(full_uri, request),
+            Ok(full_uri) => self.http_post_uri(full_uri, token, request),
             Err(err) => std::future::ready(Err(err)).boxed(),
         }
     }
@@ -162,6 +146,10 @@ pub trait FtClientHttpConnector {
     fn create_method_uri_path(&self, method_relative_uri: &str) -> ClientResult<Url> {
         Ok(FtClientHttpApiUri::create_method_uri_path(method_relative_uri).parse()?)
     }
+}
+
+lazy_static! {
+    pub static ref FT_HTTP_EMPTY_GET_PARAMS: Vec<(&'static str, Option<&'static String>)> = vec![];
 }
 
 pub struct FtClientHttpApiUri;
@@ -189,6 +177,102 @@ impl FtClientHttpApiUri {
 }
 
 #[derive(Debug)]
+pub enum FtClientError {
+    ReqwestError(FtReqwestError),
+    ApiError(FtClientApiError),
+    HttpError(FtClientHttpError),
+    HttpProtocolError(FtClientHttpProtocolError),
+    EndOfStream(FtClientEndOfStreamError),
+    SystemError(FtClientSystemError),
+    ProtocolError(FtClientProtocolError),
+    RateLimitError(FtRateLimitError),
+}
+
+impl From<FtReqwestError> for FtClientError {
+    fn from(err: FtReqwestError) -> Self {
+        FtClientError::ReqwestError(err)
+    }
+}
+
+impl From<FtClientApiError> for FtClientError {
+    fn from(err: FtClientApiError) -> Self {
+        FtClientError::ApiError(err)
+    }
+}
+
+impl From<FtClientHttpError> for FtClientError {
+    fn from(err: FtClientHttpError) -> Self {
+        FtClientError::HttpError(err)
+    }
+}
+
+impl From<FtClientHttpProtocolError> for FtClientError {
+    fn from(err: FtClientHttpProtocolError) -> Self {
+        FtClientError::HttpProtocolError(err)
+    }
+}
+
+impl From<FtClientEndOfStreamError> for FtClientError {
+    fn from(err: FtClientEndOfStreamError) -> Self {
+        FtClientError::EndOfStream(err)
+    }
+}
+
+impl From<FtClientSystemError> for FtClientError {
+    fn from(err: FtClientSystemError) -> Self {
+        FtClientError::SystemError(err)
+    }
+}
+
+impl From<FtClientProtocolError> for FtClientError {
+    fn from(err: FtClientProtocolError) -> Self {
+        FtClientError::ProtocolError(err)
+    }
+}
+
+impl From<FtRateLimitError> for FtClientError {
+    fn from(err: FtRateLimitError) -> Self {
+        FtClientError::RateLimitError(err)
+    }
+}
+
+impl FtClientError {
+    fn option_to_string<T: ToString>(value: &Option<T>) -> String {
+        value
+            .as_ref()
+            .map_or_else(|| "-".to_string(), |v| v.to_string())
+    }
+}
+
+impl Display for FtClientError {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        match *self {
+            FtClientError::ReqwestError(ref err) => err.fmt(f),
+            FtClientError::ApiError(ref err) => err.fmt(f),
+            FtClientError::HttpError(ref err) => err.fmt(f),
+            FtClientError::HttpProtocolError(ref err) => err.fmt(f),
+            FtClientError::EndOfStream(ref err) => err.fmt(f),
+            FtClientError::SystemError(ref err) => err.fmt(f),
+            FtClientError::ProtocolError(ref err) => err.fmt(f),
+            FtClientError::RateLimitError(ref err) => err.fmt(f),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct FtReqwestError {
+    pub error: reqwest::Error,
+}
+
+impl Display for FtReqwestError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Reqwest Error: {}", self.error.to_string(),)
+    }
+}
+
+impl Error for FtReqwestError {}
+
+#[derive(Debug, Builder)]
 pub struct FtClientApiError {
     pub code: String,
     pub errors: Option<Vec<String>>,
