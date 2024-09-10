48b4e27bbdeb7d37ba8f480866b0982a01f79551
Hyeokjin <tyeirdoo06@gmail.com>
2024-09-10 17:20:05 +0900

    rename api projects to project

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,8 @@
 pub mod cursus_id_projects;
 pub mod locations;
-pub mod project;
 pub mod project_sessions_id_scale_teams;
 pub mod project_sessions_id_teams;
+pub mod projects;
 pub mod users;
 pub mod users_id_correction_point_historics;
 pub mod users_id_teams;
