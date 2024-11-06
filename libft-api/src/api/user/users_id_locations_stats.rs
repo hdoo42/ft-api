53fe5991aef1526b709b6d867c2e7a81e6da5189
Hyeokjin <tyeirdoo06@gmail.com>
2024-11-06 20:35:32 +0900

    Refactor: Reorganize API module structure into subdirectories
    - 이동 작업:
      - `campus_id_locations` 및 `campus_id_users`를 `api/campus`로 이동
      - `cursus_id_projects`를 `api/cursus`로 이동
      - 프로젝트 관련 파일을 `api/project`로 이동
      - `project_session` 관련 파일을 `api/project_session`으로 이동
      - 사용자 관련 파일을 `api/user`로 이동

--- libft-api/src/api/user/users_id_locations_stats.rs
+++ libft-api/src/api/user/users_id_locations_stats.rs
@@ -1,6 +1,84 @@
+use std::collections::HashMap;
+
+use chrono::Days;
+use chrono::NaiveDate;
+use chrono::NaiveTime;
+use rsb_derive::Builder;
+use serde::{Deserialize, Serialize};
+use tracing::info;
+
 use crate::to_param;
+use crate::FtSortOption;
+use crate::{ClientResult, FtClientHttpConnector, FtClientSession, FtUserId};
+
+#[derive(Debug, Serialize, Deserialize, Builder)]
+pub struct FtApiUsersIdLocationsStatsRequest {
+    pub user_id: FtUserId,
+    pub begin_at: Option<NaiveDate>,
+    pub end_at: Option<NaiveDate>,
+    pub sort: Option<Vec<FtSortOption>>,
+    pub page: Option<u16>,
+    pub per_page: Option<u8>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Builder)]
+#[serde(transparent)]
+pub struct FtApiUsersIdLocationsStatsResponse {
+    pub stats: HashMap<NaiveDate, NaiveTime>,
+}
+
+impl<'a, FCHC> FtClientSession<'a, FCHC>
+where
+    FCHC: FtClientHttpConnector + Send + Sync,
+{
+    pub async fn users_id_locations_stats(
+        &self,
+        req: FtApiUsersIdLocationsStatsRequest,
+    ) -> ClientResult<FtApiUsersIdLocationsStatsResponse> {
+        let url = &format!("users/{}/locations_stats", req.user_id);
+
+        let params = vec![
             to_param!(req, page),
             to_param!(req, per_page),
+            (
+                "begin_at".to_string(),
+                req.begin_at.map(|date| {
+                    date.checked_add_days(Days::new(1))
+                        .map(|date| date.to_string())
+                        .expect("NaiveDate after checked add failed")
+                }),
+            ),
+            (
+                "end_at".to_string(),
+                req.end_at.map(|date| {
+                    date.checked_add_days(Days::new(1))
+                        .map(|date| date.to_string())
+                        .expect("NaiveDate after checked add failed")
+                }),
+            ),
+            (
+                "sort".to_string(),
+                req.sort.as_ref().map(|v| {
+                    v.iter()
+                        .map(|v| {
+                            format!(
+                                "{}{}",
+                                if v.descending { "-" } else { "" },
+                                serde_plain::to_string(&v.field).unwrap()
+                            )
+                        })
+                        .collect::<Vec<_>>()
+                        .join(",")
+                }),
+            ),
+        ];
+
+        self.http_session_api
+            .http_get(url, &[params].concat())
+            .await
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use chrono::{Days, Local, NaiveDate};
