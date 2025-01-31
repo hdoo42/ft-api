692437bb37432cd307d2fab8d7a07f5e7afa1423
Hyeokjin <tyeirdoo06@gmail.com>
2025-01-31 10:47:41 +0900

    chore(bin): minor update, detailed adjustment

--- libft-api/bin/teams.rs
+++ libft-api/bin/teams.rs
@@ -11,15 +11,35 @@ use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let begin_at = Utc.with_ymd_and_hms(2025, 1, 28, 7, 0, 0).unwrap();
+    let body = vec![
+        FtApiScaleTeamsMultipleCreateBody {
+            begin_at: FtDateTimeUtc::new(begin_at.clone()),
+            user_id: FtUserId::new(174094),
+            team_id: FtTeamId::new(6298862),
+        },
+        FtApiScaleTeamsMultipleCreateBody {
+            begin_at: FtDateTimeUtc::new(begin_at),
+            user_id: FtUserId::new(172309),
+            team_id: FtTeamId::new(6298846),
+        },
+    ];
+    let res = post_scale_team(body).await.unwrap();
+    println!("{res:?}");
+
+    Ok(())
+}
+
+async fn temp() {
     tracing_subscriber::fmt::init();
 
-    let evaluators = [
-        190853, 190800, 172327, 174094, 190903, 190791, 172305, 197400, 197412, 197494, 172309,
-        197539, 197459, 190817, 197605, 197606,
-    ]
-    .map(FtUserId::new);
+    let evaluators = [174094, 172309].map(FtUserId::new);
 
     let project_teams = get_project_teams().await.teams;
+
+    project_teams
+        .iter()
+        .for_each(|teams| println!("{}|{:?}", teams.id, teams.users));
 
     let begin_at = Utc.with_ymd_and_hms(2025, 1, 28, 5, 0, 0).unwrap();
     let mut bodys = Vec::new();
@@ -37,101 +57,101 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         });
     }
 
-    println!("{:?}", bodys);
-
-    let res = post_scale_team(bodys).await.unwrap();
-
-    let file_path = format!(
-        "/Users/hdoo/works/gsia/libft-api/libft-api/bin/piscine/third_cohort/first_round/rush_teams_{}.csv",
-        Utc::now().format("%Y-%m-%d_%H-%M-%S")
-    );
-
-    let mut file = std::fs::File::create(&file_path).expect("Failed to create output file");
-
-    file.write_all("project_idㅣscale_team_idㅣcreated_atㅣupdated_atㅣfinal_markㅣbegin_atㅣcorrectorㅣcorrectedsㅣfilled_atㅣtruantㅣteam.userㅣcommentㅣfeedback\n".as_bytes())?;
-
-    for scale_team in res.scale_teams {
-        let corrector = match scale_team.corrector {
-            FtCorrector::User(ft_user) => {
-                ft_user.login.map(|login| login.0).unwrap_or("".to_string())
-            }
-            FtCorrector::String(s) => s,
-        };
-        let correcteds = match scale_team.correcteds {
-            FtCorrecteds::String(s) => s,
-            FtCorrecteds::Vec(vec) => vec
-                .into_iter()
-                .map(|user| user.login.map(|l| l.0).unwrap_or("".to_string()))
-                .collect::<Vec<String>>()
-                .join(","),
-        };
-        let begin_at = match scale_team.begin_at {
-            Some(date) => date.0.to_utc().to_string(),
-            None => "".to_string(),
-        };
-        let filled_at = match scale_team.filled_at {
-            Some(date) => date.0.to_utc().to_string(),
-            None => "".to_string(),
-        };
-
-        let truant = match scale_team.truant {
-            Some(user) => user
-                .login
-                .map(|l| l.0.to_string())
-                .unwrap_or("".to_string()),
-            None => "".to_string(),
-        };
-        let (team_uesr, project_id) = match scale_team.team {
-            Some(team) => {
-                let user = team
-                    .users
-                    .map(|users| {
-                        users
-                            .into_iter()
-                            .map(|user| {
-                                user.login
-                                    .map(|l| l.0.to_string())
-                                    .unwrap_or("".to_string())
-                            })
-                            .collect::<Vec<String>>()
-                            .join(",")
-                    })
-                    .unwrap_or("".to_string());
-                let project_id = team
-                    .project_id
-                    .map(|project_id| project_id.to_string())
-                    .unwrap_or("".to_string());
-                (user, project_id)
-            }
-            None => ("".to_string(), "".to_string()),
-        };
-        let final_mark = match scale_team.final_mark {
-            Some(final_mark) => final_mark.value().to_string(),
-            None => "".to_string(),
-        };
-        writeln!(
-            file,
-            "{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{:?}ㅣ{:?}",
-            project_id,
-            scale_team.id,
-            scale_team.created_at.0.to_utc(),
-            scale_team.updated_at.0.to_utc(),
-            final_mark,
-            begin_at,
-            corrector,
-            correcteds,
-            filled_at,
-            truant,
-            team_uesr,
-            scale_team.comment,
-            scale_team.feedback
-        )
-        .expect("Failed to write record");
+    for ele in bodys.iter() {
+        println!("{},{},{}", ele.user_id, ele.team_id, ele.begin_at.value());
     }
 
-    println!("Output written to: {}", file_path);
-
-    Ok(())
+    // let res = post_scale_team(bodys).await.unwrap();
+    //
+    // let file_path = format!(
+    //     "/Users/hdoo/works/gsia/libft-api/libft-api/bin/piscine/third_cohort/first_round/rush_teams_{}.csv",
+    //     Utc::now().format("%Y-%m-%d_%H-%M-%S")
+    // );
+    //
+    // let mut file = std::fs::File::create(&file_path).expect("Failed to create output file");
+    //
+    // file.write_all("project_idㅣscale_team_idㅣcreated_atㅣupdated_atㅣfinal_markㅣbegin_atㅣcorrectorㅣcorrectedsㅣfilled_atㅣtruantㅣteam.userㅣcommentㅣfeedback\n".as_bytes())?;
+    //
+    // for scale_team in res.scale_teams {
+    //     let corrector = match scale_team.corrector {
+    //         FtCorrector::User(ft_user) => {
+    //             ft_user.login.map(|login| login.0).unwrap_or("".to_string())
+    //         }
+    //         FtCorrector::String(s) => s,
+    //     };
+    //     let correcteds = match scale_team.correcteds {
+    //         FtCorrecteds::String(s) => s,
+    //         FtCorrecteds::Vec(vec) => vec
+    //             .into_iter()
+    //             .map(|user| user.login.map(|l| l.0).unwrap_or("".to_string()))
+    //             .collect::<Vec<String>>()
+    //             .join(","),
+    //     };
+    //     let begin_at = match scale_team.begin_at {
+    //         Some(date) => date.0.to_utc().to_string(),
+    //         None => "".to_string(),
+    //     };
+    //     let filled_at = match scale_team.filled_at {
+    //         Some(date) => date.0.to_utc().to_string(),
+    //         None => "".to_string(),
+    //     };
+    //
+    //     let truant = match scale_team.truant {
+    //         Some(user) => user
+    //             .login
+    //             .map(|l| l.0.to_string())
+    //             .unwrap_or("".to_string()),
+    //         None => "".to_string(),
+    //     };
+    //     let (team_uesr, project_id) = match scale_team.team {
+    //         Some(team) => {
+    //             let user = team
+    //                 .users
+    //                 .map(|users| {
+    //                     users
+    //                         .into_iter()
+    //                         .map(|user| {
+    //                             user.login
+    //                                 .map(|l| l.0.to_string())
+    //                                 .unwrap_or("".to_string())
+    //                         })
+    //                         .collect::<Vec<String>>()
+    //                         .join(",")
+    //                 })
+    //                 .unwrap_or("".to_string());
+    //             let project_id = team
+    //                 .project_id
+    //                 .map(|project_id| project_id.to_string())
+    //                 .unwrap_or("".to_string());
+    //             (user, project_id)
+    //         }
+    //         None => ("".to_string(), "".to_string()),
+    //     };
+    //     let final_mark = match scale_team.final_mark {
+    //         Some(final_mark) => final_mark.value().to_string(),
+    //         None => "".to_string(),
+    //     };
+    //     writeln!(
+    //         file,
+    //         "{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{}ㅣ{:?}ㅣ{:?}",
+    //         project_id,
+    //         scale_team.id,
+    //         scale_team.created_at.0.to_utc(),
+    //         scale_team.updated_at.0.to_utc(),
+    //         final_mark,
+    //         begin_at,
+    //         corrector,
+    //         correcteds,
+    //         filled_at,
+    //         truant,
+    //         team_uesr,
+    //         scale_team.comment,
+    //         scale_team.feedback
+    //     )
+    //     .expect("Failed to write record");
+    // }
+    //
+    // println!("Output written to: {}", file_path);
 }
 
 async fn post_scale_team(
